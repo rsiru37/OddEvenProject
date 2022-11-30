13e083f7143e712be6e78b4105c67d9f3f2fbb52
rsiru37 <97042494+rsiru37@users.noreply.github.com>
2022-11-30 23:36:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,11 @@ import pickle
 def main():
     st.write("""
     # Checks whether the Input Number is Even/Odd
-
     This app checks even/odd status of the Input Number
     """)
     #Get Input
-
     st.header('User Input')
-
-    def user_input_features():
-        n = st.number_input("NUMBER",min_value=0,max_value=100000,step=1)
-        data = {'NUMBER': n,
-                }
-        return n
-
-    num = user_input_features()
-
+    num = st.number_input("NUMBER",min_value=0,max_value=100000,step=1)
     if(num%2 == 0):
       st.write('EVEN NUMBER')
     else:
