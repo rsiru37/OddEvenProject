0c2860bc8b590c3a37aacd82922ff8c8c71dd022
rsiru37 <97042494+rsiru37@users.noreply.github.com>
2022-11-30 23:47:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 def main():
     st.write("""
     # Checks whether the Input Number is Even/Odd
     This app checks even/odd status of the Input Number
     """)
-    #Get Input
     st.header('User Input')
     num = st.number_input("NUMBER",min_value=0,max_value=100000,step=1)
     if(num%2 == 0):
@@ -17,5 +15,5 @@ def main():
     else:
       st.write('ODD NUMBER')
     
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
