8bd734e55b209156530cb53011129f74cba11371
rsiru37 <97042494+rsiru37@users.noreply.github.com>
2022-11-30 23:28:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,28 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-st.write("""
-# Checks whether the Input Number is Even/Odd
+def main():
+    st.write("""
+    # Checks whether the Input Number is Even/Odd
 
-This app checks even/odd status of the Input Number
-""")
-#Get Input
+    This app checks even/odd status of the Input Number
+    """)
+    #Get Input
 
-st.header('User Input')
+    st.header('User Input')
 
-def user_input_features():
-    n = st.number_input("NUMBER",min_value=0,max_value=100000,step=1)
-    data = {'NUMBER': n,
-            }
-    return n
+    def user_input_features():
+        n = st.number_input("NUMBER",min_value=0,max_value=100000,step=1)
+        data = {'NUMBER': n,
+                }
+        return n
 
-num = user_input_features()
+    num = user_input_features()
 
-if(num%2 == 0):
-  st.write('EVEN NUMBER')
-else:
-  st.write('ODD NUMBER')
+    if(num%2 == 0):
+      st.write('EVEN NUMBER')
+    else:
+      st.write('ODD NUMBER')
+    
+if __name__ == '__main__':
+    main()
